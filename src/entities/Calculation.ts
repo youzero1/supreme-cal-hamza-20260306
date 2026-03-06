c91e3afac10fc6f91e32f4c64051a49063d10b9d
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-06 19:28:08 +0500

    Add src/entities/Calculation.ts

---
+++ src/entities/Calculation.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('calculations')
+export class Calculation {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text' })
+  expression!: string;
+
+  @Column({ type: 'text' })
+  result!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @Column({ type: 'boolean', default: false })
+  shared!: boolean;
+}
